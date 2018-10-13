2ecad0eca3c5640a572e4f1c56046d029a39f437
tigercosmos <phy.tiger@gmail.com>
2018-10-13 14:53:55 +0000

    ssl for https

--- main.js
+++ main.js
@@ -3,8 +3,8 @@
 const fs = require('fs');
 const http = require('http');
 const https = require('https');
-const privateKey  = fs.readFileSync('ssl/serverkey.pem', 'utf8');
-const certificate = fs.readFileSync('ssl/servercert.pem', 'utf8');
+const privateKey  = fs.readFileSync('ssl/private.key', 'utf8');
+const certificate = fs.readFileSync('ssl/ce.crt', 'utf8');
 const credentials = {key: privateKey, cert: certificate};
 const express = require('express');
 const app = express();
@@ -51,5 +51,5 @@ app.get('/', (req, res) => {
 const  httpServer = http.createServer(app);
 const  httpsServer = https.createServer(credentials, app);
 
-httpServer.listen(8000);
-httpsServer.listen(8443);
+httpServer.listen(80);
+httpsServer.listen(443);
