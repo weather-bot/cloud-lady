a22ad6096415f3172908e12d34db6ed6f04ee895
tigercosmos <phy.tiger@gmail.com>
2019-01-26 17:11:47 +0800

    update api

--- api/meow.js
+++ api/meow.js
@@ -16,8 +16,9 @@ app.post('/meow', async (req, res) => {
 
     const meow = path.join(__dirname, '../meow');
     const outputFile = path.join(__dirname, `../${uuid}.jpg`);
-    // cats is the images folder, and 27 is quantity.
-    const command = `${meow} bottom-mode cats/${Math.floor(Math.random()*48)}.jpg '${JSON.stringify(info)}' -o ${outputFile}`;
+    const imgs = fs.readdirSync(path.join(__dirname, '../cats')).filter((elm) => elm.match(/\.jpg$/));
+    const img = imgs[Math.floor(Math.random() * (imgs.length))];
+    const command = `${meow} bottom-mode ${img} '${JSON.stringify(info)}' -o ${outputFile}`;
     const {
         stdout,
         stderr
@@ -28,6 +29,32 @@ app.post('/meow', async (req, res) => {
     }
     logger.info(`/api/meow: [${uuid}] the image created.`)
     logger.info(`/api/meow: [${uuid}] return 200`);
+    return res.status(200).json({
+        uuid
+    });
+})
+
+app.post('/chinese', async (req, res) => {
+    const uuid = uuidv1();
+    logger.info(`/api/meow: [${uuid}] new request`);
+
+    const info = req.body;
+
+    const meow = path.join(__dirname, '../meow');
+    const outputFile = path.join(__dirname, `../${uuid}.jpg`);
+    const imgs = fs.readdirSync(path.join(__dirname, '../chinese-img')).filter((elm) => elm.match(/\.jpg$/));
+    const img = imgs[Math.floor(Math.random() * (imgs.length))];
+    const command = `${meow} chinese-mode ${img} '${JSON.stringify(info)}' -o ${outputFile}`;
+    const {
+        stdout,
+        stderr
+    } = await exec(command);
+    if (stderr) {
+        logger.warn(`/api/chinese: [${uuid}] return 400`);
+        return res.status(400).send("chinese creates image failed.");
+    }
+    logger.info(`/api/chinese: [${uuid}] the image created.`)
+    logger.info(`/api/chinese: [${uuid}] return 200`);
     return res.status(200).json({
         uuid
     });
